3ac45231a48b7b17c97e1f8a3951a40d25db5670
genet1212 <hhevi921@gmail.com>
2025-01-01 20:48:22 +0300

    update

--- Item-Management/src/main/java/com/backend/controller/AuthenticationController.java
+++ Item-Management/src/main/java/com/backend/controller/AuthenticationController.java
@@ -5,50 +5,43 @@ import com.backend.repository.UserRepository;
 import com.backend.service.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
     private final AuthenticationService authenticationService;
     UserRepository userRepository;
 
     @PostMapping("/register")
-    public ResponseEntity<MessageResponse> register(@RequestBody RegisterRequest request)
-    {
-        MessageResponse authRes =  authenticationService.register(request);
+    public ResponseEntity<MessageResponse> register(@RequestBody RegisterRequest request) {
+        MessageResponse authRes = authenticationService.register(request);
 //         RegisterResponse response = new RegisterResponse(
 //                 authRes.getUserName(),
 //                 authRes.getEmail(),
 //                 authRes.getRoles()
 //         );
-         return ResponseEntity.ok(authRes);
+        return ResponseEntity.ok(authRes);
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request)
-    {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         System.out.println("Request: " + request.getPassword() + request.getUserName());
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
-
-    @PutMapping("/username-update/{id}")
-    public ResponseEntity updateUserName(@PathVariable("id") long id,@Valid @RequestBody UserNameUpdateRequest usernameUpdateRequest) {
-        Optional user = userRepository.findById(id);
-        if (userRepository.existsByUserName(usernameUpdateRequest.getUserName())){
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Username is already taken!"));
-        }
-        else{
-            user.equals(usernameUpdateRequest.getUserName());
-            return ResponseEntity
-                    .ok(new MessageResponse("Username is updated!"));
-        }
-    }
-
 }
+
+
+
+
+
